2c9173c52742d29881946e646ae0313ecd55c9d8
Johnie Mason <johniemason@students.uonbi.ac.ke>
2024-07-25 08:46:26 +0300

    PLP May Cohort Frameworks

---
+++ Python_intro1.py
@@ -0,0 +1,76 @@
+# Variable declaration
+# def main():
+#     try:
+#         age = int(input("Please enter your age: "))
+
+#         if age < 12:
+#             print("Under Age!")
+#         elif 12 <= age < 18:
+#             print("Teenager!")
+#         else:
+#             print("Adult!")
+    
+#     except ValueError:
+#         print("Invalid input. Please enter a valid age.")
+    
+#     finally:
+#         print("Age verification completed.")
+
+# # Run the main program
+# if __name__ == "__main__":
+    # main()
+
+# List Comprehension
+# Squares = [x**2 for x in range  (10)]
+# print (Squares)
+
+# #For Loops
+# for i in range (5):
+#     print(f"i is i")
+
+# while loop
+# count = 0
+# while count < 5:
+#     print (f"count is {count}")
+#     count += 1 #incrementing by 1
+
+# Functions
+# def add (x, y):
+#     return x + y
+# result = add (5, 8)
+# print (result)
+
+# #Lambda Function
+# multiply = lambda x, y: x * y
+# print(multiply(3, 5))
+
+# Reading a file
+# with open('example.txt', 'r') as file:
+#     content = file.read()
+#     print(content)
+
+# Writing a file
+# with open('example.txt', 'w') as file:
+#     file.write("MASON JOHN MUNYAO, a student at THE UNIVERSITY OF NAIROBI!") 
+
+
+
+
+
+
+
+
+
+
+
+        
+        
+        
+    
+    
+
+
+
+
+
+
